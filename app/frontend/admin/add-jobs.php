c8bc7c04aac196089f496f2c4c2b818ae0a9ef19
derciosinione <derciosinione@gmail.com>
2024-05-28 16:59:45 +0100

    .

--- app/frontend/admin/add-jobs.php
+++ app/frontend/admin/add-jobs.php
@@ -50,13 +50,110 @@ $companies = $dbContext->executeSqlQuery("SELECT Id, Name FROM Companies WHERE I
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/components.css">
-    <link rel="stylesheet" href="css/add-job.css">
+    <link rel="stylesheet" href="css/add-jobs.css">
     <title>Add New Job Offer</title>
 </head>
 
 <body>
 
     <div class="board">
+        <!-- SIDE BAR -->
+        <aside>
+            <!-- SIDE BAR HEADER -->
+            <div class="sidebar-header">
+                <span>EST<span>Empregos</span></span>
+            </div>
+
+            <!-- SIDE BAR MENU -->
+            <div class="sidebar-menu">
+                <ul>
+                    <div class="active-sidebar-menu-line"></div>
+                    <li>
+                        <a href="index.php">
+                            <i class="fas fa-chart-line"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="active-sidebar-menu">
+                    <div class="active-sidebar-menu-line"></div>
+                    <li>
+                        <a href="jobs.php">
+                            <i class="fas fa-briefcase"></i>
+                            <span>Ofertas</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <br>
+                <hr>
+                <br>
+
+                <ul>
+                    <div class="active-sidebar-menu-line"></div>
+                    <li>
+                        <a href="admin.php">
+                            <i class="fas fa-user-shield"></i>
+                            <span>Admin</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <div class="active-sidebar-menu-line"></div>
+                    <li>
+                        <a href="companies.php">
+                            <i class="fas fa-building"></i>
+                            <span>Empresas</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <div class="active-sidebar-menu-line"></div>
+                    <li>
+                        <a href="team.php">
+                            <i class="fas fa-users"></i>
+                            <span>Team</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <div class="active-sidebar-menu-line"></div>
+                    <li>
+                        <a href="managers.php">
+                            <i class="fas fa-user-friends"></i>
+                            <span>Gestores</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <br>
+                <hr>
+                <br>
+
+                <ul>
+                    <div class="active-sidebar-menu-line"></div>
+                    <li><a href="settings.php">
+                            <i class="fas fa-cogs"></i>
+                            <span>Settings</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <div class="active-sidebar-menu-line"></div>
+                    <li>
+                        <a href="../website/index.php">
+                            <i class="fas fa-sign-out-alt"></i>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </aside>
         <!-- MAIN ELEMENT  -->
         <main>
             <!-- MAIN HEADER -->
